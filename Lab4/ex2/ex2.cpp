2f7637a6017104ecf87a2271cffd41c65f410deb
vilnus <90980192+Germongii@users.noreply.github.com>
2024-10-07 21:58:01 +0300

    Сделано 2 задание 4 лабараторной

---
+++ Lab4/ex2/ex2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Gun {
+private:
+    string name;
+    int damage;
+    int weight;
+public:
+    Gun(string name, int damage, int weight) {
+        this->name = name;
+        this->damage = damage;
+        this->weight = weight;
+    }
+    Gun() {
+        name = "uzi";
+        damage = 12;
+        weight = 50;
+    }
+    ~Gun() {
+        cout << name << endl;
+        cout << damage << endl;
+        cout << weight << endl;
+        cout << "Class deleting" << endl;
+    }
+};
+
+#include <iostream>
+
+int main()
+{
+    Gun* dynamicGun = new Gun("test2", 100, 10);
+    Gun customGun("test", 76, 20);
+    delete dynamicGun;
+    return 0;
+}
+
